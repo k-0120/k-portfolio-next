e8ffc87a916096a4659dbbb6d9567bf0c7c7b88d
koumura.takayuki0120 <koumura.takayuki0120@gmail.com>
2025-04-27 11:29:21 +0900

    modified skills

---
+++ src/data/skills.ts
@@ -0,0 +1,5 @@
+export const skills = [
+  { name: "Next.js", level: "初級" },
+  { name: "React", level: "初級" },
+  { name: "HTML/CSS", level: "中級" },
+];
--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,14 +1,16 @@
 import Layout from "../components/Layout";
+import { skills } from "../data/skills";
 
 export default function Skills() {
   return (
     <Layout>
       <h1 style={{ fontSize: "36px", marginBottom: "20px" }}>スキル紹介</h1>
       <ul style={{ listStyle: "none", padding: 0 }}>
-        <li>Next.js - 初級</li>
-        <li>React - 初級</li>
-        <li>HTML/CSS - 初級</li>
-        <li>Git/GitHub - 基本操作OK</li>
+        {skills.map((skill, index) => (
+        <li key={index}>
+          {skill.name} - {skill.level}
+        </li>
+        ))}
       </ul>
     </Layout>
   );
