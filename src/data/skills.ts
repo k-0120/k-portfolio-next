b2455a4abc8e6d9dba939ca5205a5a6fc0b96cca
koumura.takayuki0120 <koumura.takayuki0120@gmail.com>
2025-04-28 09:12:09 +0900

    modified skills

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,5 +1,7 @@
 export const skills = [
+  { name: "dbt", level: "中級" },
+  { name: "BigQuery", level: "中級" },
   { name: "Next.js", level: "初級" },
   { name: "React", level: "初級" },
-  { name: "HTML/CSS", level: "中級" },
+  { name: "HTML/CSS", level: "初級" },
 ];
